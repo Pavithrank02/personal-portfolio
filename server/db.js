211fa5132c8653247a2e6f5cfa48ee29820c83e9
Pavithran2 <pavithrank02@gmail.com>
2023-11-28 11:46:22 +0530

    link updated

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require("mongodb");
 // Replace the uri string with your connection string.
-// const uri = "mongodb+srv://pavithran:9790436040@cluster1.oxk2vdh.mongodb.net/t/";
+const uri = REACT_APP_LINK;
 const client = new MongoClient(uri);
 async function run() {
   try {
